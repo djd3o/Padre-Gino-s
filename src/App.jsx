15cc318cc202d313e98630584059b185d29de795
Amadeo <its.amad3o@gmail.com>
2026-07-27 18:06:22 -0700

    Padre Gino's site using react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ rootTree });
+const App = () => {
+  return (
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
