43e4aabe01f4b9212505f39f075c01ca12a9e1f1
Amadeo <its.amad3o@gmail.com>
2026-07-28 11:12:57 -0700

    Adding tanstack query devtools

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { routeTree } from "./routeTree.gen";
 
-const router = createRouter({ rootTree });
+const router = createRouter({ routeTree });
+const queryClient = new QueryClient();
 const App = () => {
   return (
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>
   );
 };
